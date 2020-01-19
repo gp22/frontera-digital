e92dd82a5ec5e80f0f4b5e44186f46f35cad5ae6
gp22 <gp22@tutanota.com>
2020-01-19 15:05:20 -0600

    add type scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontSize: {
+      body: '20px',
+      tiny: '0.555rem',
+      xs: '0.702rem',
+      sm: '0.889rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.266rem',
+      '2xl': '1.424rem',
+      '3xl': '1.602rem',
+      '4xl': '1.802rem',
+      '5xl': '2.027rem',
+      '6xl': '2.281rem',
+    },
+    extend: {},
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
