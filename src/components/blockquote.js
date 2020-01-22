2d56ae6a1f757aa7ec057ce5d38e6bf2cf3e4caf
gp22 <gp22@tutanota.com>
2020-01-22 16:11:47 -0600

    add quote-left

--- src/components/blockquote.js
+++ src/components/blockquote.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import QuoteLeft from './quote-left'
+
 const Blockquote = props => (
-  <blockquote className="bg-blue-100 border-2 border-blue-500 py-5 px-6 mb-4">
-    <p className="text-blue-600 mb-3">{props.quote}</p>
+  <blockquote className="bg-blue-100 border-2 border-blue-500 py-5 px-6 mb-4 relative">
+    <QuoteLeft className="absolute  quote-left" />
+    <p className="text-blue-600 mb-3 relative">{props.quote}</p>
     <cite className="text-blue-400 font-light">&mdash; {props.cite}</cite>
   </blockquote>
 )
