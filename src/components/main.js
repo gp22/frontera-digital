21a867a868695aed2f3f3ab76867db60f9cc5504
gp22 <gp22@tutanota.com>
2020-01-21 20:14:34 -0600

    add content

--- src/components/main.js
+++ src/components/main.js
@@ -2,42 +2,64 @@ import React from 'react'
 
 import Blockquote from './blockquote'
 
+const pClassNames = 'mb-4'
+
 const Main = () => (
-  <section className="max-w-xl">
+  <section className="max-w-xl mb-32">
     <h3 className="font-bold font-display text-2xl mb-2">
       Most businesses get their website all wrong
     </h3>
-    <p className="mb-4">
+    <p className={pClassNames}>
       They choose their layout and design based on the preferences of someone in
-      the business, and not what their target audience would find appealing.
-      They think about themselves when making design decisions, and not their
-      customers.
+      the business, and{' '}
+      <span className="font-bold">
+        not what their target audience would find appealing.
+      </span>{' '}
+      They think about <span className="italic">themselves</span> when making
+      design decisions, and <span className="italic">not their customers.</span>
     </p>
-    <p className="mb-4">
-      I get it. They probably don’t even know they’re doing this.
+    <p className={pClassNames}>
+      <span className="italic">I get it.</span> They probably don’t even know
+      they’re doing this.
     </p>
+
     <Blockquote
       quote="Website design is the main factor for 48% of users when they determine
       the credibility of a business."
       cite="Blue Corona"
     />
-    <p className="mb-4">
+
+    <p className={pClassNames}>
       One of the biggest factors people use to judge the trustworthiness of your
-      website is how it looks, and you have an incredibly short amount of time
-      (a few seconds) to grab their attention.
+      website is <span className="font-bold">how it looks</span>, and you have
+      an <span className="font-bold">incredibly short amount of time</span> (a
+      few seconds) to grab their attention.
     </p>
-    <p className="mb-4">
-      Good design is important – but what’s more important is design that speaks
-      to your audience.
+    <p className={pClassNames}>
+      <span className="italic">Good design is important</span> – but what’s more
+      important is design that speaks to your audience.
     </p>
-    <p className="mb-4">
-      And not only design that speaks to your audience, but that encourages them
-      to take action.
+    <p className={pClassNames}>
+      And not only design that speaks to your audience, but that{' '}
+      <span className="font-bold">encourages them to take action.</span>
     </p>
-    <p className="mb-4">
-      I can help you create an experience that focuses on your customers and
-      helps you achieve your goals. Together, we’ll analyze who your customers
-      are and use what we learn as a basis for your web experience.
+    <p className={pClassNames}>
+      I can help you create an experience that{' '}
+      <span className="font-bold">focuses on your customers</span> and helps you{' '}
+      <span className="font-bold">achieve your goals</span>. Together, we’ll
+      analyze who your customers are and use what we learn as a basis for your
+      web experience.
+    </p>
+
+    <Blockquote
+      quote="72% of users expect companies to have a mobile-ready website design."
+      cite="Statista"
+    />
+
+    <p>
+      Most people use their phones to browse the web these days. And they expect
+      your site to work well and provide a good experience no matter how they’re
+      viewing it.
     </p>
   </section>
 )
