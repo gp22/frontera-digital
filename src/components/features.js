cd9525faef092e91850bcc996547f8800606fd9f
gp22 <gp22@tutanota.com>
2020-01-22 22:33:48 -0600

    add overflow hidden

--- src/components/features.js
+++ src/components/features.js
@@ -15,37 +15,39 @@ const Features = () => (
   <section className="mb-24">
     <h4 className="italic">What you'll get</h4>
     <BrushStroke className="w-32  brush-stroke" />
-    <div className="mb-12  sm:flex sm:flex-wrap sm:mx-neg">
-      <Feature
-        icon={<ChatBubble />}
-        title="Proactive Communication"
-        desc="You’ll know exactly what’s going on at every step. No more waiting for emails that never arrive."
-      />
-      <Feature
-        icon={<Calendar />}
-        title="Clear deadlines and deliverables"
-        desc="My goal is to set clear deliverables, deadlines, and expectations. And meet them!"
-      />
-      <Feature
-        icon={<Brush />}
-        title="Custom-designed solution"
-        desc="I’ll deliver an experience that’s custom-tailored to your business. No cookie-cutter designs."
-      />
-      <Feature
-        icon={<Devices />}
-        title="Looks good on every device"
-        desc="To show your audience they can trust you and that yours is the business they’re looking for."
-      />
-      <Feature
-        icon={<ComputerScreen />}
-        title="Works well on mobile and desktop"
-        desc="Give your audience what they expect, and provide a great experience no matter what."
-      />
-      <Feature
-        icon={<AtomicBomb />}
-        title="Makes a killer first-impression"
-        desc="Make the most of your visitors first few seconds on your site and convince them to stay."
-      />
+    <div className="overflow-hidden">
+      <div className="mb-12  sm:flex sm:flex-wrap sm:mx-neg">
+        <Feature
+          icon={<ChatBubble />}
+          title="Proactive communication"
+          desc="You’ll know exactly what’s going on at every step. No more waiting for emails that never arrive."
+        />
+        <Feature
+          icon={<Calendar />}
+          title="Clear deadlines and deliverables"
+          desc="My goal is to set clear deliverables, deadlines, and expectations. And meet them!"
+        />
+        <Feature
+          icon={<Brush />}
+          title="Custom-designed solution"
+          desc="I’ll deliver an experience that’s custom-tailored to your business. No cookie-cutter designs."
+        />
+        <Feature
+          icon={<Devices />}
+          title="Looks good on every device"
+          desc="To show your audience they can trust you and that yours is the business they’re looking for."
+        />
+        <Feature
+          icon={<ComputerScreen />}
+          title="Works well on mobile and desktop"
+          desc="Give your audience what they expect, and provide a great experience no matter what."
+        />
+        <Feature
+          icon={<AtomicBomb />}
+          title="Makes a killer first-impression"
+          desc="Make the most of your visitors first few seconds on your site and convince them to stay."
+        />
+      </div>
     </div>
     <Squiggle className="mx-auto" />
   </section>
