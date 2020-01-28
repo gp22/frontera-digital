c5cdd2c4a867e44d56c77bb1183c2c7dcf9a428e
gp22 <gp22@tutanota.com>
2020-01-28 17:26:14 -0600

    reduce shadows

--- src/components/qualify.js
+++ src/components/qualify.js
@@ -9,7 +9,7 @@ const Qualify = () => (
       Wonder if we're a good fit to work together?
     </h4>
     <div className="lg:flex lg:wrap">
-      <div className="rounded-lg shadow-lg px-12 py-10 mb-4  lg:w-1/2 lg:mr-4 lg:mb-0">
+      <div className="rounded-lg shadow-md px-12 py-10 mb-4  lg:w-1/2 lg:mr-4 lg:mb-0">
         <h5 className="font-display mb-6 text-lg">
           I'm the right consultant for you if:
         </h5>
@@ -35,7 +35,7 @@ const Qualify = () => (
           </li>
         </ul>
       </div>
-      <div className="rounded-lg shadow-lg px-12 py-10  lg:w-1/2">
+      <div className="rounded-lg shadow-md px-12 py-10  lg:w-1/2">
         <h5 className="font-display mb-6 text-lg">
           I'm not the right consultant for you if:
         </h5>
