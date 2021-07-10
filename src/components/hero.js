21bb42bd28584045a585ac689479eb5741570f1a
Paul Garcia <gp22@users.noreply.github.com>
2021-07-10 10:32:33 -0600

    Add spacing

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,11 +4,11 @@ import ContactButton from "./contactButton"
 const Hero = () => (
   <section className="wrapper py-12 md:py-28">
     <div className="md:max-w-xl lg:max-w-2xl">
-      <h1 className="leading-tight text-3xl lg:text-display-1">
+      <h1 className="leading-tight text-3xl mb-6 lg:text-display-1">
         Modern, easy-to-use, mobile-friendly websites and apps that give you
         more of the results you want.
       </h1>
-      <p className="">
+      <p className="mb-6">
         👋{" "}
         <span className="opacity-60">
           {" "}
