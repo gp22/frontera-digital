e5d14b09142d69445368a6f03f19660e65f0fb32
Paul Garcia <gp22@users.noreply.github.com>
2021-07-11 16:22:05 -0600

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <section className="wrapper py-10 flex flex-col md:flex-row">
-      <div className="md:w-1/2">
+      <div className="md:w-1/2 md:pr-8">
         <h1 className="text-4xl">Ready to chat?</h1>
 
         <p>
@@ -24,7 +24,46 @@ const IndexPage = () => (
         </p>
         <Address className="text-sm" />
       </div>
-      <div className="md:w-1/2"></div>
+      <div className="md:w-1/2">
+        <form name="contact" method="POST" data-netlify="true">
+          <div className="my-4">
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="w-full border px-4 py-3"
+              />
+            </label>
+          </div>
+          <div className="my-4">
+            <label>
+              Email
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="w-full border px-4 py-3"
+              />
+            </label>
+          </div>
+          <div className="my-4">
+            <label>
+              Message
+              <textarea
+                name="message"
+                id="message"
+                rows="5"
+                className="w-full border px-4 py-3"
+              />
+            </label>
+          </div>
+          <button type="submit" className="btn">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   </Layout>
 )
