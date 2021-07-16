43372750ef17d6d1038f44f460c5e89ea8a1480a
Paul Garcia <gp22@users.noreply.github.com>
2021-07-15 19:12:45 -0600

    add function

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const fetch = require("node-fetch")
+const { EMAIL_API_KEY } = process.env
+
+exports.handler = async event => {
+  const email = JSON.parse(event.body).payload.email
+  console.log(`Recieved a submission: ${email}`)
+  return fetch("https://api.sendinblue.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "api-key": `${EMAIL_API_KEY}`,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Submitted to Sendinblue:\n ${data}`)
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+}
