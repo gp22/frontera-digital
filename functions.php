1ecefca544e5b5fef3dd8f500bda2b8d2a2f0de8
Paul Garcia <gp22@users.noreply.github.com>
2022-02-07 20:10:43 -0600

    Add WordPress FSE theme files

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Set theme version to global variable.
+ * @package vibe
+ * @since 0.0.1
+ */
+define( 'VERSION', wp_get_theme()->get( 'Version' ) );
+
+// Enqueue stylesheets
+require_once 'inc/enqueues.php';
+
+// Register post types
+require_once 'inc/post-types.php';
---
+++ inc/enqueues.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Enqueue Styles for theme front end.
+ *
+ * @package industrial
+ * @since 0.0.1
+ */
+if ( ! function_exists( 'wf_styles' ) ) :
+
+    /**
+     * Enqueue Styles.
+     *
+     * @return void
+     */
+    function wf_styles() {
+
+        // Register theme stylesheet
+        wp_register_style(
+            'wf-style',
+            get_theme_file_uri( 'style.css' ),
+            array(),
+            VERSION
+        );
+
+        // Add styles inline.
+        wp_add_inline_style( 'wf-style', wf_get_font_face_styles() );
+
+        // Enqueue theme stylesheet
+        wp_enqueue_style( 'wf-style');
+    }
+    add_action( 'wp_enqueue_scripts', 'wf_styles' );
+
+endif;
+
+if ( ! function_exists( 'wf_editor_styles' ) ) :
+
+    /**
+     * Enqueue editor styles.
+     *
+     * @return void
+     */
+    function wf_editor_styles() {
+
+        // Add styles inline.
+        wp_add_inline_style( 'wp-block-library', wf_get_font_face_styles() );
+    }
+    add_action( 'admin_init', 'wf_editor_styles' );
+
+endif;
+
+if ( ! function_exists( 'wf_get_font_face_styles' ) ) :
+
+    /**
+     * Get font face styles.
+     * Called by wf_styles() & wf_editor_styles().
+     *
+     * @return string
+     */
+    function wf_get_font_face_styles(): string {
+
+      return "
+        @font-face {
+          font-family: 'Poppins';
+          font-weight: 400;
+          font-style: normal;
+          font-stretch: normal;
+          src: url('" . get_theme_file_uri( 'assets/fonts/Poppins-Regular.ttf' ) . "') format('truetype');
+        }
+
+        @font-face {
+          font-family: 'Poppins';
+          font-weight: 400;
+          font-style: italic;
+          font-stretch: normal;
+          src: url('" . get_theme_file_uri( 'assets/fonts/Poppins-Italic.ttf' ) . "') format('truetype');
+        }
+
+        @font-face {
+          font-family: 'Poppins';
+          font-weight: 700;
+          font-style: normal;
+          font-stretch: normal;
+          src: url('" . get_theme_file_uri( 'assets/fonts/Poppins-Bold.ttf' ) . "') format('truetype');
+        }
+
+        @font-face {
+          font-family: 'Poppins';
+          font-weight: 700;
+          font-style: italic;
+          font-stretch: normal;
+          src: url('" . get_theme_file_uri( 'assets/fonts/Poppins-BoldItalic.ttf' ) . "') format('truetype');
+        }
+      ";
+    }
+endif;
+
+/**
+ * Removing wp-emoji extra unnecessary loading.
+ *
+ * @since 0.0.3
+ */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
